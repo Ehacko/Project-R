e45b45e33713a40b5197f686d018b33270ecadbe
TDJDEV <TDJDEV@.github.com>
2019-11-13 17:40:06 -0400

    plein du pug

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  options={};
+  options.Date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
   res.render('index', {
     Title: 'RESTHOME',
+    Options: options,
     ParamsGet: req.query
   });
 });
