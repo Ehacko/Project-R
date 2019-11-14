d2023442561199650976cb16fee9cc7c0ae568e3
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2019-11-14 12:38:52 -0500

    .mongo

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,16 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 
 /**/
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const ObjectId    = require('mongodb').ObjectID;
+const assert      = require('assert');
 
-const ObjectId = require('mongodb').ObjectID;
-
-const url = 'mongodb+srv://ludji:root@test-w7rhz.mongodb.net/test?retryWrites=true&w=majority';
-
-const dbName = 'Project-R';
 const client = new MongoClient(url, { useNewUrlParser: true });
-/* */
 
-const findDoc = require('../tests/app');
-
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connection établie");
-
-
-
-});
-const db = client.db(dbName);
-const collIngredient = db.collection('ingredients');
-
+const url    = 'mongodb+srv://ludji:root@test-w7rhz.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'Project-R';
+/**/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -38,18 +24,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/test', function(req, res, next) {
-
-  const gogogo = async _ => { return db.collection('ingredients').find({}).sort({ Nom: 1 }).toArray(); };
-
-  gogogo().then(value => {
-    //client.close(); 
-
-    res.render('indextest', {
-      Title: 'RESTHOME',
-      ParamsGet: req.query,
-      a: value
-    });
-  }).catch((err) => { 'query error : ' + console.log(err); });
+  client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Connection établie");
+    const db = client.db(dbName);
+    const collIngredient = db.collection('ingredients');
+  
+    const gogogo = async _ => { return db.collection('ingredients').find({}).sort({ Nom: 1 }).toArray(); };
+  
+    gogogo().then(value => {
+      //client.close(); 
+  
+      res.render('indextest', {
+        Title: 'RESTHOME',
+        ParamsGet: req.query,
+        a: value
+      });
+    }).catch((err) => { 'query error : ' + console.log(err); });
+  });
 });
 
 
