0dfdf9d3ea7984dd3c43093ca1f8410e72027455
TDJDEV <TDJDEV@.github.com>
2019-11-15 17:53:01 -0400

    a

--- routes/index.js
+++ routes/index.js
@@ -4,48 +4,49 @@ const router = express.Router();
 //const querinator = require('../tests/app');
 const querinator = require('../query').querinator;
 
-
+const ojtIf = function (a,b) {
+  if(a) return a;
+  else return b;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   let J = new Date(new Date().toDateString()).valueOf()
-  if(req.query.recepices){
+  if(req.query.Recepices || req.query.recepices){
     coltarget="recettes";
-    if(req.query.recepices.toLowerCase()=="all") findtarget={};
-    else findtarget={Nom: req.query.recepices};
+    if(req.query.Recepices.toLowerCase()=="all" || req.query.Recepices.toLowerCase()=="all") findtarget={};
+    else findtarget={Nom: ojtIf(req.query.Recepices,req.query.recepices)};
   }
-  else if(req.query.ingredients){
+  else if(req.query.Ingredients ||req.query.ingredients){
     coltarget="ingredients";
-    if(req.query.ingredients.toLowerCase()=="all") findtarget={};
-    else findtarget={Nom: req.query.ingrédients};
+    if(req.query.Ingredients.toLowerCase()=="all" || req.query.ingredients.toLowerCase()=="all") findtarget={};
+    else findtarget={Nom: ojtIf(req.query.Ingredients, req.query.ingredients) };
   }
-  else if(req.query.search) coltarget=false;
+  else if(req.query.Search || req.query.search) coltarget=false;
   else {
     coltarget="menus";
-    if(req.query.menus == "" || req.query.menus == undefined ) findtarget={ Date: J};
-    else{
-      if(req.query.menus.toLowerCase() == "all") findtarget={};
-      else findtarget={Date: req.query.menus};
+    if(req.query.menus || req.query.Menus){
+      if(req.query.Menus.toLowerCase() == "all" || req.query.menus.toLowerCase() == "all") findtarget={};
+      else findtarget={Date: ojtIf(req.query.Menus,req.query.menus)};
     }
+    else findtarget={ Date: J};
   }
-  cl(coltarget);
   if(coltarget){
 
     const gogogo = async _ => { return querinator.findItem(coltarget, findtarget);};
     gogogo().then(value => {
 
-      cl(value);
       res.render('index', {
 
         Title: 'RESTHOME',
         ParamsGet: req.query,
-        a: value
+        db: value
 
       });
 
     }).catch((err) => { 'query error : ' + console.log(err); });
-    
+
   } else {
     res.render('index', {
       Title: 'RESTHOME',
